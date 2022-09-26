56f6f925b56e041117398e466ef5f2e877e27418
Maxim Bondarenco <Maxim.Bondarenco@endava.com>
2022-09-26 10:33:07 +0300

    moved request to api folder and created gameApi service, changed global config options for react-query, created instance of axios, implemented load more functionality

---
+++ src/api/index.ts
@@ -0,0 +1,1 @@
+export * as gameApi from './gameApi'
--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -2,24 +2,32 @@ import React, {FC, useEffect, useState} from 'react';
 import styles from './Homepage.module.scss'
 import GameCard, {IGame} from "../../components/GameCard/GameCard";
 import {useQuery} from "@tanstack/react-query";
-import axios from "axios";
 import Loader from "../../components/Loader/Loader";
 import {useAppDispatch} from "../../store";
 import {setCount} from "../../store/features/games";
+import {gameApi} from './../../api/index'
 
 const Homepage: FC = () => {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState<any>([])
+    const [page, setPage] = useState(1)
     const dispatch = useAppDispatch()
 
     const {isLoading, error, data} = useQuery(['all games'],
-        () => axios.get(`${import.meta.env.VITE_API_URL}games?key=${import.meta.env.VITE_API_KEY}&page_size=40`))
+        () => gameApi.getGames(), {
+            onSuccess: (data) => {
+                setPage(page => page + 1)
+                setGames(data?.data.results)
+                dispatch(setCount(data?.data.count))
+            }
+        })
 
-    useEffect(() => {
-        if(data) {
-            setGames(data?.data?.results)
-            dispatch(setCount(data?.data?.count))
-        }
-    }, [data])
+    const fetchMore = () => {
+        gameApi.getGames(40, page).then(({data}: any) => {
+            setPage(page => page + 1)
+            setGames([...games, ...data?.results])
+            console.log(data)
+        })
+    }
 
     return (
         <div className={styles.homepage}>
@@ -29,46 +37,13 @@ const Homepage: FC = () => {
             </div>
 
             <div className={styles.cardsWrapper}>
-                <div className={`${styles.loader} ${data && styles.loaded}`}>
+                <div className={`${styles.loader} ${!isLoading && styles.loaded}`}>
                     {isLoading && <Loader/>}
                 </div>
 
-                <div className={`${styles.cards} ${data && styles.cardsFetched}`}>
+                <div className={`${styles.cards} ${!isLoading && styles.cardsFetched}`}>
                     <div className={styles.cardsColumn}>
-                        {games?.slice(0, 10).map((item: IGame) => (
-                            <GameCard key={item.id}
-                                      added={item.added}
-                                      name={item.name}
-                                      background_image={item.background_image}
-                                      metacritic={item.metacritic}
-                                      platforms={item.platforms}
-                            />
-                        ))}
-                    </div>
-                    <div className={styles.cardsColumn}>
-                        {games?.slice(10, 20).map((item: IGame) => (
-                            <GameCard key={item.id}
-                                      added={item.added}
-                                      name={item.name}
-                                      background_image={item.background_image}
-                                      metacritic={item.metacritic}
-                                      platforms={item.platforms}
-                            />
-                        ))}
-                    </div>
-                    <div className={styles.cardsColumn}>
-                        {games?.slice(20, 30).map((item: IGame) => (
-                            <GameCard key={item.id}
-                                      added={item.added}
-                                      name={item.name}
-                                      background_image={item.background_image}
-                                      metacritic={item.metacritic}
-                                      platforms={item.platforms}
-                            />
-                        ))}
-                    </div>
-                    <div className={styles.cardsColumn}>
-                        {games?.slice(30, 40).map((item: IGame) => (
+                        {games?.map((item: IGame) => (
                             <GameCard key={item.id}
                                       added={item.added}
                                       name={item.name}
@@ -79,6 +54,7 @@ const Homepage: FC = () => {
                         ))}
                     </div>
                 </div>
+                <button onClick={fetchMore}>load more</button>
             </div>
 
 
