28515700b07b3427fa7428b9d036f239e8638633
Maxim Bondarenco <Maxim.Bondarenco@endava.com>
2022-09-30 10:52:37 +0300

    deleted homepage folder in pages

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,44 +1,33 @@
-import React, {FC, useEffect, useRef, useState} from 'react';
-import styles from '../components/homepage/Homepage.module.scss'
-import GameCard, {IGame} from "../components/GameCard/GameCard";
+import React, {FC, useEffect, useState} from 'react';
+import styles from './Homepage.module.scss'
+import GameCard, {IGame} from "../../components/GameCard/GameCard";
 import {useQuery} from "@tanstack/react-query";
-import axios from "axios";
-import Loader from "../components/Loader/Loader";
-import {useAppDispatch} from "../store";
-import {setCount} from "../store/features/games";
-import {Masonry} from "../components/Masonry/Masonry";
-import Isotope from 'isotope-layout';
+import Loader from "../../components/Loader/Loader";
+import {useAppDispatch} from "../../store";
+import {setCount} from "../../store/features/games";
+import {gameApi} from './../../api/index'
 
 const Homepage: FC = () => {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState<any>([])
+    const [page, setPage] = useState(1)
     const dispatch = useAppDispatch()
-    const isotope = useRef<Isotope | null>(null)
-
-    useEffect(() => {
-        isotope.current = new Isotope('.cards', {
-            itemSelector: '.card__wrapper',
-            layoutMode: 'masonry',
-            masonry: {
-                columnWidth: 104,
-                gutter: 20,
-            },
-        })
-
-        // cleanup
-        return () => isotope?.current?.destroy()
-    }, [games])
 
     const {isLoading, error, data} = useQuery(['all games'],
-        () => axios.get(`${import.meta.env.VITE_API_URL}games?key=${import.meta.env.VITE_API_KEY}&page_size=38`), {
-            refetchOnWindowFocus: false
+        () => gameApi.getGames(), {
+            onSuccess: (data) => {
+                setPage(page => page + 1)
+                setGames(data?.data.results)
+                dispatch(setCount(data?.data.count))
+            }
         })
 
-    useEffect(() => {
-        if(data) {
-            setGames(data?.data?.results)
-            dispatch(setCount(data?.data?.count))
-        }
-    }, [data])
+    const fetchMore = () => {
+        gameApi.getGames(40, page).then(({data}: any) => {
+            setPage(page => page + 1)
+            setGames([...games, ...data?.results])
+            console.log(data)
+        })
+    }
 
     return (
         <div className={styles.homepage}>
@@ -48,17 +37,12 @@ const Homepage: FC = () => {
             </div>
 
             <div className={styles.cardsWrapper}>
-                <div className={`${styles.loader} ${data && styles.loaded}`}>
+                <div className={`${styles.loader} ${!isLoading && styles.loaded}`}>
                     {isLoading && <Loader/>}
                 </div>
 
-                <div className={`cards ${styles.cards} ${data && styles.cardsFetched}`} ref={isotope}>
-
-                    {/*<Masonry dataArray={games}*/}
-                    {/*         columnCount={4}*/}
-                    {/*         ChildsElement={GameCard}*/}
-                    {/*/>*/}
-
+                <div className={`${styles.cards} ${!isLoading && styles.cardsFetched}`}>
+                    <div className={styles.cardsColumn}>
                         {games?.map((item: IGame) => (
                             <GameCard key={item.id}
                                       added={item.added}
@@ -66,25 +50,11 @@ const Homepage: FC = () => {
                                       background_image={item.background_image}
                                       metacritic={item.metacritic}
                                       platforms={item.platforms}
-                                      ref={isotope}
                             />
                         ))}
-
-
-
-                        {/*<div className={styles.cardsColumn}>*/}
-                        {/*    {games?.map((item: IGame) => (*/}
-                        {/*        <GameCard key={item.id}*/}
-                        {/*                  added={item.added}*/}
-                        {/*                  name={item.name}*/}
-                        {/*                  background_image={item.background_image}*/}
-                        {/*                  metacritic={item.metacritic}*/}
-                        {/*                  platforms={item.platforms}*/}
-                        {/*        />*/}
-                        {/*    ))}*/}
-                        {/*</div>*/}
-
+                    </div>
                 </div>
+                <button onClick={fetchMore}>load more</button>
             </div>
 
 
